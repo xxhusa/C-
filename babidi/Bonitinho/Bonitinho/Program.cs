e2fa69209ad167b8895af28f1065b87ea80f99d7
unknown <lab1069@etecjk.local>
2025-08-05 19:17:09 -0300

    aulads1

---
+++ babidi/Bonitinho/Bonitinho/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonitinho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Qual é o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine("Boa noite, " + nome);
+        }
+    }
+}
