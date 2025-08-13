4eb7b17841b6f8a6924fab3b669e6b94e3b15354
unknown <lab1069@etecjk.local>
2025-08-12 22:26:54 -0300

    projeto2

---
+++ projeto2/projeto2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            pessoa objJogador;
+            pessoa camisa10;
+            pessoa camisa7;
+
+            objJogador = new pessoa();
+            camisa10 = new pessoa();
+            camisa7 = new pessoa();
+
+            objJogador.cadastrar();
+            camisa10.cadastrar();
+            camisa7.cadastrar();
+
+            objJogador.Exibir();
+            camisa10.Exibir();
+            camisa7.Exibir();
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ projeto2/projeto2/pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto2
+{
+    class pessoa
+    {
+        public string nome;
+        public string email;
+        public string telefone;
+
+        public void cadastrar() {
+
+        Console.WriteLine("Digite o nome do jogador:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu E-mail:");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Digite seu telefone:");
+            telefone = Console.ReadLine();
+
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine("nome: {0}" ,nome);
+            Console.WriteLine("email : {0}" ,email);
+            Console.WriteLine("telefone : {0}" ,telefone);
+        }
+
+    }
+}
